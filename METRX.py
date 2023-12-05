71eca1f47ddeae74a08229561c1ad1e823336601
nKKai <46084374+nKiux@users.noreply.github.com>
2023-12-05 22:51:55 +0800

    v0.3 update

--- METRX.py
+++ METRX.py
@@ -1,17 +1,26 @@
 import tkinter as tk
 import psutil
+import asyncio
+import time
 
-def windowscontrol():
+async def windowcontrol():
+    global leng
+    global usuage
     usuage = psutil.cpu_percent(1)
     leng = int(screen_width * usuage / 100)
     print(usuage)
-    if leng < 200:
-        root.geometry(f"240x0")
-    else:
-        root.geometry(f"{leng}x0")
+    if leng < 240:
+        leng = 240
     root.title(f"CPU:{usuage}% || METRX")
     
-    root.after(50, windowscontrol)
+    root.update()
+    #root.after(10, windowcontrol)
+    await windupdt()
+    await time.sleep(100)
+
+async def windupdt():
+    root.geometry(f"{leng}x0")
+    await windowcontrol()
 
 root = tk.Tk()
 root.call('wm', 'attributes', '.', '-topmost', True)
@@ -19,5 +28,8 @@ screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 root.geometry(f"+0+0")
 root.iconbitmap('MX.ico')
-windowscontrol()
+asyncio.run(windowcontrol())
+asyncio.run(windupdt())
 root.mainloop()
+
+
