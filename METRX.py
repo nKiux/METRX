e83e8e34be878a1ec652a7d118fda60266c00ce8
nKKai <46084374+nKiux@users.noreply.github.com>
2023-12-06 08:52:43 +0800

    v0.3 bug fix reupload

--- METRX.py
+++ METRX.py
@@ -15,7 +15,7 @@ async def windowcontrol():
         root.title(f"CPU:{usuage}% || METRX")
         root.update()
         #root.after(10, windowcontrol)
-        await time.sleep(100)
+        time.sleep(0.1)
         root.geometry(f"{leng}x0")
         await windowcontrol()
 
@@ -24,7 +24,7 @@ root.call('wm', 'attributes', '.', '-topmost', True)
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 root.geometry(f"+0+{screen_height - 100}")
-root.iconbitmap('MX.ico')
+#root.iconbitmap('MX.ico')
 asyncio.run(windowcontrol())
 root.mainloop()
 
